7f11be8f2ce4ce9b171dc1bea10df34a64824f2a
xcruzaguilera <xochitl.cruz-aguilera@sony.com>
2023-01-30 10:15:46 -0800

    Additional notes

--- across-the-streams/client/src/App.js
+++ across-the-streams/client/src/App.js
@@ -6,6 +6,7 @@ import Footer from './Margins/footer';
 import MainPg from './Components/mainPg';
 
 function App() {
+  //useContext Hook works in multiple layers of components. It is configured in the main file (App.js - highest level),so we are able to reuse in any other components, to avoid props drilling, which can get messy.
   //useState to store the accessToken
   const [accessToken, setAccessToken] = useState(null);
 
@@ -19,8 +20,9 @@ function App() {
     //const namedFunction = () => {};
     //useEffect(namedFunction, []);
     
-    //Should only run once because of dependency array
-    //Look up: Component Div Mount
+    //Should only run once because of dependency array....runs once because of the dependecy array being empty
+    //How getAccessToken is being saved into the useState, it will update all it's children
+    //It is being updated via built in methods such as useState, useState will receive update and then gives the updates to its children, updating the values.
     getAccessToken()
       .then(keyToken => setAccessToken(keyToken))
       .catch(console.error)
@@ -44,3 +46,23 @@ function App() {
 }
 
 export default App;
+
+/*
+NOTES:
+useContext - sets global state to your component; Info is passed top down only
+Provider - Passes the value, provides you the value
+Comnsumer - use with class components
+
+APP.JS
+Not exectuted until it's used(referenced)
+App.js is the CORE component and the entry point for React
+App.js runs first, since React is read top down, when it gets to Line 3 it accessses api.js to get the info and the createContext.
+componentDivMount lifecyle completes, then we can use useEffect()
+Once everything is mounted, before App.js can be considered mounted THEN useEffect() is triggered. 
+Only then can you run async operations which will return a promise(.then & .catch)
+
+API.JS
+getAccessToken is called after the 1st render, any async function needs to run after mounted
+Render(s) and component(s) run synchronously. 
+
+*/
--- across-the-streams/client/src/Components/api.js
+++ across-the-streams/client/src/Components/api.js
@@ -6,8 +6,7 @@ const CLIENT_SECRET = process.env.REACT_APP_TWITCH_CLIENT_SECRET;
 
 //Do not need accessToken variable because using will be using useContext Hook
 // let accessToken;
-//useContext Hook works in multiple layers of components. It is created in the main file (App.js - highest level),
-// so we are able to reuse in any other components, to avoid props drilling, which can get messy.
+
 //accessToken will be used multiple files, we need to EXPORT the APIContext variable, export is placed at the beginning od declared variable so it can be exported and used in other files.
 //createContext: Call it only ONCE & at a central location
 //useContext will be used everywhere else in the project
@@ -31,16 +30,17 @@ export const getAccessToken = async () => {
         );
         //Do not need accessToken because it will be using useContext Hook. 
         //UseContext Hook will help 
-        // accessToken = res.data;
+        // accessToken = res.data; data in the result
         return res.data;
     } catch (error) {
-        //Note: error is an object, generic, and it won't always get you the info on what's wrong
+        //Note: error is an object, and it won't always get you the info on what's wrong
         console.log(error);
         //Note: error.message is standard practice, that will return the error message, error description that should explain what error happened
         console.log(error.message);
         //?????????????ASK QUESTIONS*******************************
 
         //Specify to return null, otherwise it will return undefined by default. State value is set to null, so it's better to stay consistent.
+        //set state in app.js.....look at getAccessToken & where the value is getting saved(in this case the usestate)
         return null;
     }
 };
