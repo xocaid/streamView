87407f3a049d0b45957a0c44b817416520d24538
xcruzaguilera <xochitl.cruz-aguilera@sony.com>
2023-01-30 14:29:06 -0800

    fetchData added, not working

--- across-the-streams/client/src/Components/mainPg.js
+++ across-the-streams/client/src/Components/mainPg.js
@@ -2,51 +2,69 @@ import { useEffect, useState, useContext } from 'react';
 import SingleStream from './singleStream';
 import { APIContext } from './api';
 
+
 function MainPg() {
     //useContext: to read APIContext. Provider
-    const {accessToken} = useContext(APIContext);
+    const { accessToken } = useContext(APIContext);
     const [streams, setStreams] = useState([]);
     const [searchStreams, setSearchStreams] = useState([]);
     console.log(accessToken);
 
     //FETCHING DATA - TWITCH TV API
-    // const fetchStreams = () => {
-    //     fetch('https://api.twitch.tv/helix/search/channels')
-    //         .then((response) => response.json())
-    //         .then((data) => {
-    //             console.log(data);
-    //             setSearchStreams(data);
-    //             setStreams(data);
-    //         });
-    // };
-
-    // useEffect(fetchStreams, []);
-
-    //FILTER FUNCTION - SEARCH BAR
-    const filterStreams = event => {
-        const value = event.target.value.toLowerCase();
-        const filteredStreams = searchStreams.filter(
-            stream => (`${stream.name} ${stream.extra.address}`
-                .toLowerCase()
-                .includes(value))
+    const fetchData = () => {
+        fetch('https://api.twitch.tv/helix/search/channels',
+            {
+                headers: {
+                    Authorization: `Bearer accessToken`,
+                    'Client-Id': process.env.CLIENT_ID
+                }
+            }
         )
-        //setStreams(filteredStreams) displays filtered streams from search bar
-        setStreams(filteredStreams)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                setSearchStreams(data);
+                setStreams(data);
+            });
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+    if (!streams) {
+        return <div>
+            Loading..
+        </div>;
+    } else {
+
+
+
+        //FILTER FUNCTION - SEARCH BAR
+        const filterStreams = event => {
+            const value = event.target.value.toLowerCase();
+            const filteredStreams = searchStreams.filter(
+                stream => (`${stream.name} ${stream.extra.address}`
+                    .toLowerCase()
+                    .includes(value))
+            )
+            //setStreams(filteredStreams) displays filtered streams from search bar
+            setStreams(filteredStreams)
+        }
+
+        return (
+            <div>MainPg Div
+                <div>
+                    Search Bar Div
+                    <input className='search-bar' placeholder='Search Streams...' onInput={filterStreams}></input>
+                </div>
+
+                <div>
+                    Stream Info Div
+                    <SingleStream />
+                </div>
+            </div>
+        )
     }
-
-    return (
-        <div>MainPg Div
-            <div>
-                Search Bar Div
-                <input className='search-bar' placeholder='Search Streams...' onInput={filterStreams}></input>
-            </div>
-
-            <div>
-                Stream Info Div
-                <SingleStream />
-            </div>
-        </div>
-    )
 }
 
 export default MainPg;
